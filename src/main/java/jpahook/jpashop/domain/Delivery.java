dd754eb27d7e6622c05b15319a7deaa3fb3f10e1
rimrim990 <lim990718@naver.com>
2022-08-28 14:35:55 +0900

    [fix] add getter and setter

--- src/main/java/jpahook/jpashop/domain/Delivery.java
+++ src/main/java/jpahook/jpashop/domain/Delivery.java
@@ -1,8 +1,12 @@
 package jpahook.jpashop.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 
 @Entity
+@Getter @Setter
 public class Delivery {
 
     @Id @GeneratedValue
