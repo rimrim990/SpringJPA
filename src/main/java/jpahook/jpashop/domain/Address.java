f4aa0448941a46ddcf53236166c89a9dd11c204d
rimrim990 <lim990718@naver.com>
2022-08-28 13:46:25 +0900

    [fix] 값 타입 immutable 하게 수정

--- src/main/java/jpahook/jpashop/domain/Address.java
+++ src/main/java/jpahook/jpashop/domain/Address.java
@@ -1,15 +1,25 @@
 package jpahook.jpashop.domain;
 
 import lombok.Getter;
-import lombok.Setter;
 
 import javax.persistence.Embeddable;
 
 @Embeddable
-@Getter @Setter
+@Getter
 public class Address {
 
     private String city;
     private String street;
     private String zipcode;
+
+    // jpa
+    protected Address() {
+    }
+
+    // immutable 하게 생성
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
