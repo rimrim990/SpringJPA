6368d5fc14ab7991e89403f5123018cf7c13cc1d
rimrim990 <lim990718@naver.com>
2022-08-28 13:46:46 +0900

    [feat] add entity

---
+++ src/main/java/jpahook/jpashop/domain/Category.java
@@ -0,0 +1,36 @@
+package jpahook.jpashop.domain;
+
+import jpahook.jpashop.domain.item.Item;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Category {
+
+    @Id @GeneratedValue
+    @Column(name = "category_id")
+    private Long id;
+
+    private String name;
+
+    @ManyToMany
+    @JoinTable(name = "category_item",
+            // 중간 테이블
+            joinColumns = @JoinColumn(name = "category_id"),
+            inverseJoinColumns = @JoinColumn(name = "item_id"))
+    // many to many 를 일대 다 관계로 풀어내는 중간 테이블 필요
+    // 실전에서는 쓰지 말자 - 중간 테이블 수정이 불가능하고 운영하기 어렵다고 함
+    private List<Item> items = new ArrayList<>();
+
+    @ManyToOne
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> child = new ArrayList<>();
+}
