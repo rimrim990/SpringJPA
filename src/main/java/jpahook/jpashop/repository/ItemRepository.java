0302fe4fec4f7db5d4613867c1247c8b458e8daf
rimrim990 <lim990718@naver.com>
2022-08-28 16:26:25 +0900

    [feat] add item repository and service

---
+++ src/main/java/jpahook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,34 @@
+package jpahook.jpashop.repository;
+
+import jpahook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    public void save(Item item) {
+        if (item.getId() == null) {
+            // 새로 생성된 객체
+            em.persist(item);
+        } else {
+            // 이미 db 에 등록된 객체
+            em.merge(item);
+        }
+    }
+
+    public Item findOne(Long id) {
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i", Item.class)
+                .getResultList();
+    }
+}
